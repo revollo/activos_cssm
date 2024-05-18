050d54b93d41e4fda2353c8bde6c6573f56d00ca
revollo <borbogecssm@gmail.com>
2024-05-18 10:16:32 -0400

    conexion.js

--- conexion.js
+++ conexion.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //mongoose.connect("mongodb://127.0.0.1:27017/cssm_activos").then(() => {
 
-mongoose.connect("mongodb+srv://fijos:VOxHQhWJRtzAMuO3@cluster0.xsmzyab.mongodb.net/fijos").then(() => {
+mongoose.connect("mongodb+srv://fijos:VOxHQhWJRtzAMuO3@cluster0.xsmzyab.mongodb.net/activos").then(() => {
 
     console.log("Conectado a la base de datos");
 }).catch((error) => {
